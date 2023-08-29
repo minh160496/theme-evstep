c5c2215ec38cd48a906a5b30d7e524d678d7628d
minh160496 <leminh16041996@gmail.com>
2023-08-29 15:21:12 -0700

    change env variable

--- src/app/tin-tuc/[slug]/page.tsx
+++ src/app/tin-tuc/[slug]/page.tsx
@@ -4,7 +4,8 @@ import { Post } from "@/features/post";
 import { getPost } from "../page";
 
 export async function generateStaticParams() {
-  const api_url = "http://localhost/wordpress/wp-json/wp/v2";
+  const api_url =
+    process.env.API_URL || "http://127.0.0.1/wordpress/wp-json/wp/v2";
   const posts = await fetch(`${api_url}/posts`, {
     next: { revalidate: 10 },
   }).then((res) => res.json());
